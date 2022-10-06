478776736ef2b663e966f7cf6c90c62a639216eb
immprada <im.mprada@gmail.com>
2022-10-05 20:43:04 -0500

    solve cardholder

--- resources/scripts/binding-with-image.js
+++ resources/scripts/binding-with-image.js
@@ -1,6 +1,7 @@
+const CARDHOLDER_DEFAULT = 'JANE APPLESED';
 const imageCardHolderData = document.getElementById("image-card-holder-data");
 
 const bindingCardHolderDataData = (value) => {
-
-  imageCardHolderData.innerHTML = value;
+  if(value == '') return imageCardHolderData.innerHTML = CARDHOLDER_DEFAULT;
+  imageCardHolderData.innerHTML = value.toUpperCase();
 }
