11699fd1ad5e27c8cab9983bd594f9f807536326
immprada <im.mprada@gmail.com>
2022-10-05 20:56:00 -0500

    solve card number

--- resources/scripts/binding-with-image.js
+++ resources/scripts/binding-with-image.js
@@ -1,7 +1,16 @@
 const CARDHOLDER_DEFAULT = 'JANE APPLESED';
+const CARDNUMBER_DEFAULT = '0000 0000 0000 0000';
 const imageCardHolderData = document.getElementById("image-card-holder-data");
+const imageCardNumberData = document.getElementById("image-card-number-data");
 
 const bindingCardHolderDataData = (value) => {
   if(value == '') return imageCardHolderData.innerHTML = CARDHOLDER_DEFAULT;
   imageCardHolderData.innerHTML = value.toUpperCase();
+}
+
+const bindingCardNumberDataData = (value) => {
+  if(value == '') return imageCardNumberData.innerHTML = CARDNUMBER_DEFAULT;
+
+  maskedValue = value.match(/.{1,4}/g).join(' ');
+  imageCardNumberData.innerHTML = maskedValue;
 }
